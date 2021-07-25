615f5edb6f81cc20ba7934d4b92c49a2c4fa3185
DzcGood <920402784@qq.com>
2021-07-26 00:33:38 +0800

    day01

---
+++ day01/src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * @author DzcGood
+ * @date 2021/7/25 - 23:41
+ */
+public class HelloWorld {
+    //main方法，程序入口
+    //单行注释
+    /*
+    多行注释
+    多行注释
+    多行注释
+    * */
+    public static void main(String[] args) {
+        //单行注释
+        System.out.println("Hello, World!");
+    }
+}
