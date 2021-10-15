8f50f9eadb65eef047f71c25eb3a6160bc07bfa1
DzcGood <920402784@qq.com>
2021-10-15 14:22:39 +0800

    TCP连接例子

--- day24/test/TCPTest.java
+++ day24/test/TCPTest.java
@@ -1,9 +1,6 @@
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -16,10 +13,11 @@ import java.net.Socket;
 /*
 * 实现TCP的网络编程
 * 例子1：客户端发送信息给服务端，服务端将数据显示在控制台上
-*
-*   > 2、获取
-* */
+* 例子2：客户端发送文件给服务端，服务端把文件存储在本地
+* 例子3：客户端发送文件给服务端，服务端把文件存储在本地，并返回“发送成功给客户端”
+ * */
 public class TCPTest {
+    //  例子1：客户端发送信息给服务端，服务端将数据显示在控制台上
     //客户端
     @Test
     public void client() throws IOException {//记得用try-catch-finally,懒得写了。。。。
@@ -63,6 +61,89 @@ public class TCPTest {
         System.out.println(baos.toString());
         //5、所有资源都需要关闭
         baos.close();
+        inputStream.close();
+        socket.close();
+        serverSocket.close();
+    }
+
+
+    //例子2：客户端发送文件给服务端，服务端把文件存储在本地
+    @Test
+    public void client1() throws IOException{//记得用try-catch-finally
+        Socket socket = new Socket(InetAddress.getByName("127.0.0.1"),9090);
+        OutputStream outputStream = socket.getOutputStream();
+        FileInputStream fis = new FileInputStream("flower.jpg");
+        byte[] buffer = new byte[1024];
+        int len;
+        while((len = fis.read(buffer))!= -1){
+            outputStream.write(buffer, 0 , len);
+        }
+        fis.close();
+        outputStream.close();
+        socket.close();
+    }
+
+    @Test
+    public void server1()throws IOException{//记得try-catch-finally
+        ServerSocket serverSocket = new ServerSocket(9090);
+        Socket socket = serverSocket.accept();
+        InputStream inputStream = socket.getInputStream();
+        FileOutputStream fos = new FileOutputStream("flower_copy.jpg");
+        byte[] buffer = new byte[1024];
+        int len;
+        while((len = inputStream.read(buffer)) != -1){
+            fos.write(buffer, 0, len);
+        }
+        fos.close();
+        inputStream.close();
+        socket.close();
+        serverSocket.close();
+    }
+
+//例子3：客户端发送文件给服务端，服务端把文件存储在本地，并返回“发送成功给客户端”
+
+    @Test
+    public void client2() throws IOException{//记得用try-catch-finally
+        Socket socket = new Socket(InetAddress.getByName("127.0.0.1"),9090);
+        OutputStream outputStream = socket.getOutputStream();
+        FileInputStream fis = new FileInputStream("flower.jpg");
+        byte[] buffer = new byte[1024];
+        int len;
+        while((len = fis.read(buffer))!= -1){
+            outputStream.write(buffer, 0 , len);
+        }
+        //关闭数据的输出
+        socket.shutdownOutput();//********************没有此语句的话，会报错******************
+        InputStream inputStream = socket.getInputStream();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        buffer = new byte[5];
+        while((len = inputStream.read(buffer)) != -1){
+            baos.write(buffer, 0 ,len);
+        }
+        System.out.println(baos.toString());
+        inputStream.close();
+        baos.close();
+        fis.close();
+        outputStream.close();
+        socket.close();
+    }
+
+    @Test
+    public void server2()throws IOException{//记得try-catch-finally
+        ServerSocket serverSocket = new ServerSocket(9090);
+        Socket socket = serverSocket.accept();
+        InputStream inputStream = socket.getInputStream();
+        FileOutputStream fos = new FileOutputStream("flower_copy2.jpg");
+        byte[] buffer = new byte[1024];
+        int len;
+        while((len = inputStream.read(buffer)) != -1){
+            fos.write(buffer, 0, len);
+        }
+        //服务器向客户机发送数据
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write("照片已收到".getBytes());
+        outputStream.close();
+        fos.close();
         inputStream.close();
         socket.close();
         serverSocket.close();
